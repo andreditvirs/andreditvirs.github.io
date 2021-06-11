aa6605114d9410e04fd9a481f0629a95b0269762
AndreDitVir01 <novaandre.saputraadit@gmail.com>
2021-06-11 16:16:49 +0700

    FIX: retrieve data for hidden-shorten-link with mongodb, strapi, cloudinary, github-pages

--- shorten-url/index2.js
+++ shorten-url/index2.js
@@ -1,2 +1,23 @@
-const [ , , subdomain] = window.location.hostname.split(".").reverse();
-console.log(subdomain);
+let search = window.location.search.substr(1);
+if(search){
+    const api_url = "https://strapi-andreditvirs.herokuapp.com/shorten-urls/" + search;
+    async function getApi(url) {
+        try{
+            const response = await fetch(url).then((response) => {
+                if (response.status >= 400 && response.status < 600) {
+                    throw new Error("Bad response from server");
+                }
+                return response;
+            });
+
+            var data = await response.json();
+            if(confirm(`Anda akan membuka laman bernama ${data.name} (${data.description})`)){
+                window.open(data.url, "_self");
+            }
+        } catch (e){
+            alert("Maaf, halaman tidak ditemukan");
+        }
+    }
+    getApi(api_url);
+
+}
